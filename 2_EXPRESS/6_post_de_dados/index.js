64953f55b8e6b20b99b2267ba6523b8ffcf6d2ea
Cauã <caueiracouto@gmail.com>
2023-10-02 19:27:53 -0300

    atualização de arquivos

--- 2_EXPRESS/6_post_de_dados/index.js
+++ 2_EXPRESS/6_post_de_dados/index.js
@@ -10,10 +10,15 @@ app.get('/users/cadastrar', (req, res) => {
 
 app.get('/users/:id', (req, res) => {
     const id = req.params.id
+})
+app.post('/users/save', (req, res)=>{
+    console.log(req.body.name)
+
+})
 
     console.log(`Estamos buscando pelo usuario: ${id}`)
     res.sendFile(`${caminho}/usuarios.html`)
-}) 
+ 
 
 app.get ('/', (req, res) => {
     res.sendFile(`${caminho}/index.html`)
